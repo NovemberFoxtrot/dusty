d30239cf48946595f73696c0db0eb1847e5b7a5d
November Foxtrot <november@fox.trot>
2014-02-01 23:58:16 +0900

    fun

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define MAX_LISTEN_BACKLOG 1
+#define BUFFER_SIZE 4096
+
+void handle_client_connection(int client_socket_fd, char *backend_host, char* backend_port_str) {
+	struct addrinfo hints;
+	struct addrinfo *addrs;
+	struct addrinfo *addrs_iter;
+
+	int backend_socket_fd;
+
+	char buffer[BUFFER_SIZE];
+
+	int bytes_read;
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	getaddrinfo = getaddrinfo(backend_host, backend_port_str, &hints, &addrs);
+
+	if (getaddrinfo_error != 0) {
+		fprintf(stderr, "could not find backend: %s\n", gai_strerror(getaddrinfo_error));
+		exit(1);
+	}
+
+	for (addrs_iter = addrs; addrs_iter != NULL; addr_iter = addr_iter->ai_next) {
+		backend_socket_fd = socket(addrs_iter->ai_family, addrs_iter->ai_socktype, addrs_iter->ai_protocol);
+
+		if (backend_socket_fd == -1) {
+			continue;
+		}
+
+		if (connect(backend_socket_fd, addrs_iter->ai_addr, addrs_iter->ai_addrlen) != -1) {
+			break;
+		}
+
+		close(backend_socket_fd);
+	}
+
+	if (addrs_iter == NULL) {
+		fprintf(stderr, "could not connect to the backend\n");
+		exit(1);
+	}
+
+	freeaddrinfo(addrs);
+
+	bytes_read = read(client_socket_fd, buffer, BUFFER_SIZE);
+
+	write(backend_socket_fd, buffer, bytes_read);
+
+	while (bytes_read = read(backend_socket_fd, buffer, BUFFER_SIZE)) {
+		write(client_socket_fd, buffer, bytes_read);
+	}
+
+	close(client_socket_fd);
+}
+
+
